560449e01031af91038fc8d6c66a8753e58baba6
Ian Synge <gamersynge@gmail.com>
2025-07-16 16:23:07 -0400

    Updated component and hook

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,17 @@
 import { useLocalStorage } from "../hooks/useLocalStorage";
 function Form() {
+    const [name, setName] = useLocalStorage("name", "");
+    const [serviceNumber, setServiceNumber] = useLocalStorage("serviceNumber", "");
 
     return (
       <>
         <form style={{ display: "flex", flexDirection: "column" }}>
             <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
+            <input type="text" data-testid={"name"} 
+            value={name} onChange={(e) => setName(e.target.value)}/>
             <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
+            <input type="text" data-testid={"service"} 
+            value={serviceNumber} onChange={(e) => setServiceNumber(e.target.value)}/>
 
         </form>
         <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [value, setValue] = useState(() => {
+        const saved = localStorage.getItem(key);
+        return saved !== null ? saved : initialValue;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [key, value]);
+
+  return [value, setValue];
 }
